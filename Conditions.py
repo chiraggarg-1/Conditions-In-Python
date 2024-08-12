00717e03c65b9988032fdc593cd028d1f583da19
CHIRAGGARG1697 <chiraggarg.0106@gmail.com>
2024-08-12 19:55:19 +0530

    First Commit

---
+++ Conditions.py
@@ -0,0 +1,17 @@
+x = 2
+print(x==2)
+print(x==3)
+print(x<3)
+name = "Chirag Garg"
+age = 27
+if name=="Chirag Garg" and age== 27:
+    print("His age is Chirag Garg and he is 27 years old")
+if name == "Chirag Garg" or name == "monu":
+    print("His name is Chirag Garg as well as Monu")
+
+    name = "Monu"
+    if name in ["Chirag","Monu"]:
+        print("Your name either is John or Rick")
+
+        print(not False)
+        print((not False)== True)
